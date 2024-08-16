41b6d9beaed9c6d4c7fdd6a920083e7f18e2265e
Vrishti A <vaprojectcs@gmail.com>
2024-08-16 12:57:43 +0000

    Written calculate function

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,7 +2,24 @@ import numpy as np
 
 def calculate(list):
 
+    #Checking list contains 9 numbers, otherwise raising ValueError.
+    if len(list) != 9:
+        raise ValueError("List must contain nine numbers.")
+    
+    #Converting into 3x3 NumPy Array
+    matrix = np.array(list).reshape((3,3))
+    
+    #Creating Dictionary
+    properties = ['mean', 'var', 'std', 'max', 'min', 'sum']
+    operations = [0, 1, None]
+    calculations = dict() #or {}
 
+    for p in properties:
+        calculations[p] = [] #Initially Empty Value
 
+        for o in operations:
+            calculations[p].append(
+                (getattr(matrix, p)(axis=o)).tolist()
+            )
 
     return calculations
